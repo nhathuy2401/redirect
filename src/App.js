7b33564ed3f06b581befaf7b1b8ec36796d15dc9
nhathuy2401 <nhathuy2401@gmail.com>
2024-07-10 17:57:03 +0700

    redirect after 2s

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const RedirectComponent = () => {
     // Đặt timeout để điều hướng sau 3 giây
     const timer = setTimeout(() => {
       window.location.href = 'https://abdicatebirchcoolness.com/zybg77jbb?key=0db27c22cd10202766cd5c5858cffa88';
-    }, 1000);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
